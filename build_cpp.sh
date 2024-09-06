e5d949af3f5322f03e78798cd78ae7b94e4bd952
lfpc <luisfelipe1998@gmail.com>
2024-09-06 15:04:30 +0200

    Adjust general definitions

--- build_cpp.sh
+++ build_cpp.sh
@@ -1,7 +1,6 @@
 export PYTHONPATH=$PYTHONPATH:`readlink -f python`:`readlink -f cpp/build`
-python3 chore/find_cmake_command.py
 cd cpp/
 cd build
-cmake -Dpybind11_DIR=/users/lpratesc/.local/lib/python3.11/site-packages/pybind11/share/cmake/pybind11 -DPython_EXECUTABLE=/usr/bin/python3 ..
+cmake -Dpybind11_DIR=/home/hep/lprate/.local/lib/python3.11/site-packages/pybind11/share/cmake/pybind11 -DPython_EXECUTABLE=/usr/bin/python3 ..
 make -j
 cd ../..
